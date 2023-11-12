24e1ad714062fa06d793f2e96ba1ac9519e02f33
Min Theim Kyaw <mintheimkyaw12@gmail.com>
2023-11-12 22:17:42 +0630

    [add]add contact and about data

--- src/app/aboutus/page.tsx
+++ src/app/aboutus/page.tsx
@@ -3,6 +3,10 @@ export default function Aboutus() {
       <main className="py-4">
         <section className="py-12 flex flex-col items-center text-center gap-8">
           <h1 className="text-4xl font-bold">About Us</h1>
+          <p className="text-2xl text-muted-foreground">
+            This is currency exchange website. Calculate from any currency to mmk. Check first 5 currency rate with bar chart and donut chart.
+            Reference from Central Bank of Myanmar ( <a href="https://forex.cbm.gov.mm/">https://forex.cbm.gov.mm/</a> ).
+          </p>
         </section>
       </main>
     )
--- src/app/contactus/page.tsx
+++ src/app/contactus/page.tsx
@@ -3,6 +3,10 @@ export default function Contactus() {
     <main className="py-4">
       <section className="py-12 flex flex-col items-center text-center gap-8">
         <h1 className="text-4xl font-bold">Contact Us</h1>
+          <p className="text-2xl text-muted-foreground">
+            Name: Mr Min Theim Kyaw <br/>
+            Email:   mintheimkyaw12@gmail.com
+          </p>
       </section>
     </main>
   )
